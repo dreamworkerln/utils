2bf7f0e1f215381ecb1f6d0c894b43692c8c8632
hatuncev <hatuncev.ku@kvant-telecom.ru>
2020-03-17 19:31:12 +0300

    First commit

---
+++ src/main/java/ru/kvant_telecom/tv/utils/rest/RestTemplateFactory.java
@@ -0,0 +1,172 @@
+package ru.kvant_telecom.tv.utils.rest;
+
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.ProtocolException;
+import org.apache.http.client.RedirectStrategy;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultRedirectStrategy;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.protocol.HttpContext;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.SSLContext;
+import java.nio.charset.Charset;
+import java.security.cert.X509Certificate;
+
+public class RestTemplateFactory {
+
+
+    /**
+     * Custom header, contains request (last redirected) URL - allow get redirect url
+     */
+    public static final String X_LAST_URI = "X-LastURL";
+    private static final int TIMEOUT = 5000;
+    private static final boolean CHECK_CERT = true;
+    private static final boolean THROW_ON_ERROR = true;
+
+
+
+    public static RestTemplate getRestTemplate() {
+
+        return getRestTemplateInternal(CHECK_CERT, THROW_ON_ERROR, TIMEOUT);
+    }
+
+    public static RestTemplate getRestTemplate(int timeout) {
+
+        return getRestTemplateInternal(CHECK_CERT, THROW_ON_ERROR, timeout);
+    }
+
+    public static RestTemplate getRestTemplate(boolean throwOnError, int timeout) {
+
+        return  getRestTemplateInternal(CHECK_CERT, throwOnError, timeout);
+    }
+
+
+    public static RestTemplate getRestTemplate(boolean checkCert, boolean throwOnError) {
+
+        return  getRestTemplateInternal(checkCert, throwOnError, TIMEOUT);
+    }
+
+    public static RestTemplate getRestTemplate(boolean checkCert, boolean throwOnError, int timeout) {
+
+        return  getRestTemplateInternal(checkCert, throwOnError, timeout);
+    }
+
+
+    
+
+    private static RestTemplate getRestTemplateInternal(boolean checkCert, boolean throwOnError, int timeout) {
+
+
+        RestTemplate result;
+
+
+        RedirectStrategy redirectStrategy = new DefaultRedirectStrategy() {
+
+            //private String lastRedirect = null;
+
+            @Override
+            public boolean isRedirected(HttpRequest request, HttpResponse response,
+                                        HttpContext context) throws ProtocolException {
+
+                boolean isRedirected;
+                //System.out.println(response);
+
+                isRedirected = super.isRedirected(request, response, context);
+
+                // If redirect intercept intermediate response.
+                if (isRedirected) {
+                    //int statusCode  = response.getStatusLine().getStatusCode();
+                    //System.out.println("redirectURL: " + redirectURL);
+                    //lastRedirect = response.getFirstHeader("Location").getValue();
+                }
+                else {
+//                    if (lastRedirect != null) {
+//                        response.setHeader(X_LAST_URI, lastRedirect);
+//                    }
+
+                }
+
+                response.setHeader(X_LAST_URI, request.getRequestLine().getUri());
+
+                return isRedirected;
+            }
+        };
+
+
+        if (!checkCert) {
+
+
+            try {
+
+                TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+
+                SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom()
+                        .loadTrustMaterial(null, acceptingTrustStrategy)
+                        .build();
+
+
+                RequestConfig requestConfig = RequestConfig.custom()
+                        .setConnectTimeout(timeout)
+                        .setConnectionRequestTimeout(timeout)
+                        .setSocketTimeout(timeout)
+                        .build();
+
+
+                SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
+                
+                CloseableHttpClient httpClient = HttpClients.custom()
+                        .setDefaultRequestConfig(requestConfig)
+                        .setSSLSocketFactory(csf)
+                        .setRedirectStrategy(redirectStrategy)
+                        .build();
+
+
+
+                HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+
+                requestFactory.setConnectTimeout(timeout);
+                requestFactory.setConnectionRequestTimeout(timeout);
+                requestFactory.setReadTimeout(timeout);
+                requestFactory.setHttpClient(httpClient);
+
+                result = new RestTemplate(requestFactory);
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        else {
+
+            CloseableHttpClient httpClient = HttpClients.custom()
+                    .setRedirectStrategy(redirectStrategy)
+                    .build();
+            
+            HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+            requestFactory.setConnectionRequestTimeout(timeout);
+            requestFactory.setConnectTimeout(timeout);
+            requestFactory.setReadTimeout(timeout);
+            requestFactory.setHttpClient(httpClient);
+
+            result = new RestTemplate(requestFactory);
+        }
+
+        // Force using UTF-8 as default character encoding
+        result.getMessageConverters()
+                .add(0, new StringHttpMessageConverter(Charset.forName("UTF-8")));
+
+        // Add custom errorHandler that suppress any exceptions
+        if (!throwOnError) {
+            result.setErrorHandler(new RestTemplateResponseErrorHandler());
+        }
+
+        return result;
+    }
+
+}
---
+++ src/main/java/ru/kvant_telecom/tv/utils/rest/RestTemplateResponseErrorHandler.java
@@ -0,0 +1,300 @@
+package ru.kvant_telecom.tv.utils.rest;
+
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.client.ResponseErrorHandler;
+import java.io.IOException;
+
+import static org.springframework.http.HttpStatus.Series.CLIENT_ERROR;
+import static org.springframework.http.HttpStatus.Series.SERVER_ERROR;
+
+
+/**
+ * RestTemplate Error handler - by default only suppress exception (client and resourceserver)
+ */
+class RestTemplateResponseErrorHandler
+        implements ResponseErrorHandler {
+
+    @Override
+    public boolean hasError(ClientHttpResponse httpResponse)
+            throws IOException {
+
+        return (
+                httpResponse.getStatusCode().series() == CLIENT_ERROR||
+                httpResponse.getStatusCode().series() == SERVER_ERROR);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public void handleError(ClientHttpResponse httpResponse)throws IOException {
+
+
+        if (httpResponse.getStatusCode().series() == SERVER_ERROR) {
+            // handle SERVER_ERROR
+
+        }
+        else if (httpResponse.getStatusCode().series() == CLIENT_ERROR) {
+            // handle CLIENT_ERROR
+        }
+    }
+}
+
+
+
+
+
+
+// ==========================================================================
+
+
+
+
+
+//        else if (httpResponse.getStatusCode().series() == CLIENT_ERROR) {
+//                // handle CLIENT_ERROR
+//
+//            /*
+//            reading response as string
+//
+//            String s = new BufferedReader(new InputStreamReader(httpResponse.getBody()))
+//                    .lines().collect(Collectors.joining());
+//
+//            System.out.println(s);
+//            throw new RuntimeException(s);
+//
+//            if (httpResponse.getStatusCode() == HttpStatus.NOT_FOUND) {
+//                //throw new NotFoundException("Not found");
+//            }
+//            */
+//                }
+
+
+
+
+//
+//
+///**
+// * Bult-in curl, allowed enabling/disabling ssl(tls) сertificates check and custom timeouts(default 5000 ms)
+// */
+//@SuppressWarnings({"WeakerAccess", "unused"})
+//class Rest {
+//
+///*    protected static final int TIMEOUT = 5000;
+//
+//    // protected String userAgent = "ass-with-ears-2.0";
+//
+//    //protected Credentials credentials = null;
+//
+//    protected static final boolean CHECK_SERT = true;
+//
+//    protected static final boolean THROW_ON_ERROR = true;
+//
+//    // protected RestTemplate restTemplate;
+//
+//    //protected Map<String, String> customHeaders = new HashMap<>();
+//
+//    public RestTemplate getRestTemplate() {
+//
+//        return RestTemplateFactory.getRestTemplate(CHECK_SERT, THROW_ON_ERROR, TIMEOUT);
+//    }
+//
+//    public RestTemplate getRestTemplate(int timeout) {
+//
+//        return RestTemplateFactory.getRestTemplate(CHECK_SERT, THROW_ON_ERROR, timeout);
+//    }
+//
+//    public RestTemplate getRestTemplate(boolean throwOnError, int timeout) {
+//
+//        //this.timeout = timeout;
+//        return  RestTemplateFactory.getRestTemplate(CHECK_SERT, throwOnError, timeout);
+//    }
+//
+//
+//    public RestTemplate getRestTemplate(boolean checkCert, boolean throwOnError) {
+//
+//        return  RestTemplateFactory.getRestTemplate(checkCert, throwOnError, TIMEOUT);
+//    }
+//
+//    public RestTemplate getRestTemplate(boolean checkCert, boolean throwOnError, int timeout) {
+//
+//        return  RestTemplateFactory.getRestTemplate(checkCert, throwOnError, timeout);
+//    }*/
+//
+////
+////    /**
+////     * Perform get request
+////     * @param url Url
+////     * @return ResponseEntity<String>
+////     */
+////    public static ResponseEntity<String> get(RequestEntity<String> request, RestTemplate template) {
+////
+////        return template.exchange(request, String.class);
+////    }
+////
+////    /**
+////     * Perform get request
+////     * @param url Url
+////     * @return ResponseEntity<T>
+////     */
+////    public static <T,R> ResponseEntity<R> get(RequestEntity<T> request, RestTemplate template, Class<R> responseType) {
+////
+////        return template.exchange(request, responseType);
+////    }
+//
+//
+////    /**
+////     * Perform get request
+////     * @param url Url
+////     * @return ResponseEntity<T>
+////     */
+////    public ResponseEntity<T> get(String url, Class<T> responseType) {
+////
+////        System.out.println(responseType.getSimpleName());
+////
+////        ResponseEntity<T> result;
+////
+////        //Set the headers you need send
+////        final HttpHeaders headers = getHeaders();
+////        //headers.set("User-Agent", userAgent);
+////
+////        //Create a new HttpEntity
+////        HttpEntity<String> entity = new HttpEntity<>(headers);
+////
+////        restTemplate.getFor
+////
+////        result = restTemplate.getForEntity(url, responseType);
+////
+////        return result;
+////    }
+//
+//
+//
+//
+//
+//
+//
+////    /**
+////     * Perform get request
+////     * @param url Url
+////     * @return ResponseEntity<byte[]>
+////     */
+////    public ResponseEntity<byte[]> download(String url, HttpHeaders headers, RestTemplate restTemplate) {
+////
+////        ResponseEntity<byte[]> result;
+////
+////        HttpHeaders headers = getHeaders();
+////        //headers.set("User-Agent", userAgent);
+////        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_OCTET_STREAM));
+////
+////        HttpEntity<String> entity = new HttpEntity<>(headers);
+////        result = restTemplate.exchange(url, HttpMethod.GET, entity, byte[].class);
+////
+////        return result;
+////    }
+//
+//
+//
+////    /**
+////     *
+////     * @param url Url
+////     * @param data String
+////     * @return ResponseEntity<String>
+////     */
+////    public ResponseEntity<String> post(String url, String data, HttpHeaders headers, RestTemplate restTemplate) {
+////
+////        ResponseEntity<String> result;
+////
+////        //Set the headers you need send
+////        HttpHeaders headers = getHeaders();
+////        headers.setContentType(MediaType.APPLICATION_JSON);
+////
+////        //Create a new HttpEntity
+////        HttpEntity<String> entity = new HttpEntity<>(data, headers);
+////
+////        result = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
+////
+////        return result;
+////    }
+//
+//
+////
+////    /**
+////     * Perform post request
+////     * @param url Url
+////     * @return ResponseEntity<T>
+////     */
+////    public ResponseEntity<T> post(String url, HttpHeaders headers, RestTemplate restTemplate, Class<T> responseType) {
+////
+////        ResponseEntity<T> result;
+////
+////        //Set the headers you need send
+////        final HttpHeaders headers = getHeaders();
+////
+////        //Create a new HttpEntity
+////        HttpEntity<String> entity = new HttpEntity<>(headers);
+////        headers.setContentType(MediaType.APPLICATION_JSON);
+////
+////        restTemplate.exchange()
+////
+////        result =  restTemplate.exchange(url, HttpMethod.POST, entity, responseType);
+////        return result;
+////    }
+//
+//
+//// -------------------------------------------------------------------------
+//
+//
+////    public static addCredentials(HttpHeaders headers) {
+////
+////        String auth = credentials.getUserPrincipal().getName() + ":" + credentials.getPassword();
+////        String encodedAuth = Base64.encodeBase64String(auth.getBytes(Charset.forName("US-ASCII")));
+////
+////        String authHeader = "Basic " + encodedAuth;
+////        result.set("Authorization", authHeader);
+////
+////
+////
+////    }
+//
+//    /*
+//
+//    protected HttpHeaders getHeaders() {
+//
+//        HttpHeaders result = new HttpHeaders();
+//
+//        // first apply custom headers
+//        customHeaders.forEach(result::set);
+//
+//        // then add User-Agent
+//        result.set("User-Agent", userAgent);
+//
+//        //https://www.baeldung.com/how-to-use-resttemplate-with-basic-authentication-in-spring
+//        if (credentials != null) {
+//
+//            String auth = credentials.getUserPrincipal().getName() + ":" + credentials.getPassword();
+//            String encodedAuth = Base64.encodeBase64String(auth.getBytes(Charset.forName("US-ASCII")));
+//
+//            String authHeader = "Basic " + encodedAuth;
+//            result.set("Authorization", authHeader);
+//
+//            //byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")) );
+//            //String authHeader = "Basic " + new String(encodedAuth);  "YWRtaW46YWRtaW4=";
+//        }
+//
+//        return result;
+//    }
+//    */
+//
+//
+//
+//
+//
+//// -------------------------------------------------------------------------
+//
+//
+////    public String getUserAgent() {
+////        return userAgent;
+////    }
+////
+////    public void setUserAgent(String userAgent) {
+////        this.userAgent = userAgent;
+////    }
